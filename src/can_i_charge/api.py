02835943859eb1c0b2561e3f789a3689fd7574b3
Bram Oosterlynck <booster@inuits.eu>
2025-02-19 20:44:16 +0100

    Use shellrecharge python package
    The
    [python-shellrecharge](https://github.com/cyberjunky/python-shellrecharge)
    simplifies this package a lot

--- src/can_i_charge/api.py
+++
@@ -1,7 +0,0 @@
-from shellrecharge_api import ShellRechargeApi
-
-class Api:
-    def get_api(self, api_name):
-        return {
-            "shellrecharge": ShellRechargeApi()
-        }.get(api_name)
