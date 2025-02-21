93c76caee413c6cf3997514350a5452f3563ef45
Gijs Vermeulen <gijsvrm@gmail.com>
2025-02-21 09:39:55 +0100

    Optimize imports & use format strings consistently

--- src/can_i_charge/__main__.py
+++ src/can_i_charge/__main__.py
@@ -1,13 +1,12 @@
-import asyncio
-import click
-
+from asyncio import run
 from can_i_charge.charging_status import get_charging_status
+from click import command, option
 
 
-@click.command()
-@click.option("-s", "--station", envvar="STATIONS", multiple=True)
+@command()
+@option("-s", "--station", envvar="STATIONS", multiple=True)
 def main(station):
-    asyncio.run(get_charging_status(station))
+    run(get_charging_status(station))
 
 
 if __name__ == "__main__":
--- src/can_i_charge/charging_status.py
+++ src/can_i_charge/charging_status.py
@@ -1,11 +1,8 @@
-import aiohttp
-import click
-import shellrecharge
-
-from asyncio import CancelledError
+from aiohttp import ClientSession
 from aiohttp.client_exceptions import ClientError
-from shellrecharge import LocationEmptyError, LocationValidationError
-
+from asyncio import CancelledError
+from click import echo
+from shellrecharge import Api, LocationEmptyError, LocationValidationError
 
 status_icon_map = {
     "occupied": "🚫",
@@ -14,22 +11,22 @@ status_icon_map = {
 
 
 async def get_charging_status(location_ids):
-    async with aiohttp.ClientSession() as session:
-        api = shellrecharge.Api(session)
+    async with ClientSession() as session:
+        api = Api(session)
         for location_id in location_ids:
             try:
                 location = await api.location_by_id(location_id)
-                click.echo(f"Status for station {location.address.streetAndNumber}, {location.address.postalCode} {location.address.city}")
+                echo(
+                    f"Status for station {location.address.streetAndNumber}, {location.address.postalCode} {location.address.city}"
+                )
                 for evses in location.evses:
                     status_icon = status_icon_map.get(evses.status.lower(), "❓")
-                    click.echo(
+                    echo(
                         f"Connector {evses.uid} is {evses.status.lower()} {status_icon}"
                     )
             except LocationEmptyError:
-                click.echo(f"No data returned for {location_id}, check location id")
+                echo(f"No data returned for {location_id}, check location id")
             except LocationValidationError as err:
-                click.echo(
-                    "Location validation error {}, report location id".format(err)
-                )
-            except (ClientError, TimeoutError, CancelledError) as err:
-                click.echo(err)
+                echo(f"Location validation error {err}, report location id")
+            except (CancelledError, ClientError, TimeoutError) as err:
+                echo(err)
