0b67c38202bf25758b166aae1213ba9b73a96298
Bram Oosterlynck <bram.oosterlynck@gmail.com>
2026-01-13 07:39:46 +0100

    Always create event loop
    fixes #10

--- src/can_i_charge/__main__.py
+++ src/can_i_charge/__main__.py
@@ -1,4 +1,4 @@
-from asyncio import get_event_loop, run
+from asyncio import new_event_loop, run
 from can_i_charge.cli import get_charging_status
 from can_i_charge.exporter import run_metrics_loop
 from click import command, option, version_option
@@ -16,7 +16,7 @@ def main(exporter, interval, port, station, verbose):
     if exporter:
         start_http_server(port if port else 9041)
         metrics_loop = run_metrics_loop(station, interval if interval else 60)
-        get_event_loop().run_until_complete(metrics_loop)
+        new_event_loop().run_until_complete(metrics_loop)
     else:
         run(get_charging_status(station, verbose))
 
